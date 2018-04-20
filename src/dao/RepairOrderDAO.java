bb5b8edc00287a421b41dd748bfdc4869245a246
Keona Rollerson <keonarollerson@gmail.com>
2018-04-19 20:03:46 -0500

    f

--- src/dao/RepairOrderDAO.java
+++ src/dao/RepairOrderDAO.java
@@ -42,14 +42,6 @@ public class RepairOrderDAO {
         PreparedStatement stmt = null;
         try {
             stmt = conn.prepareStatement("insert into repair order values (?, ?, ?, ?, ?, ?, ?)");
-
-            stmt.setInt(1, repairOrder.shipOut_CID);
-            stmt.setInt(2, repairOrder.shipIn_CID);
-            stmt.setInt(3, repairOrder.EID);
-            stmt.setInt(4, repairOrder.RID);
-            stmt.setString(5, repairOrder.dateRecd);
-            stmt.setString(6, repairOrder.dateShipped);
-            stmt.setString(7, repairOrder.shipOutType);
             stmt.setInt(1, repairOrder.getShipOut_CID());
             stmt.setInt(2, repairOrder.getShipIn_CID());
             stmt.setInt(3, repairOrder.getEID());
@@ -67,7 +59,7 @@ public class RepairOrderDAO {
         PreparedStatement stmt = null;
         try {
             stmt = conn.prepareStatement("delete from repair order where RID = ?");
-            stmt.setInt(1, repairOrder.RID);
+            
             stmt.setInt(1, repairOrder.getRID());
             stmt.execute();
         } finally {
@@ -85,13 +77,7 @@ public class RepairOrderDAO {
                     + "EID = ?,"
                     + "shipIn_CID = ?"
                     + "where RID = ?");
-            stmt.setInt(1, repairOrder.shipOut_CID);
-            stmt.setInt(2, repairOrder.shipIn_CID);
-            stmt.setInt(3, repairOrder.EID);
-            stmt.setInt(4, repairOrder.RID);
-            stmt.setString(5, repairOrder.dateRecd);
-            stmt.setString(6, repairOrder.dateShipped);
-            stmt.setString(7, repairOrder.shipOutType);
+            
             stmt.setInt(1, repairOrder.getShipOut_CID());
             stmt.setInt(2, repairOrder.getShipIn_CID());
             stmt.setInt(3, repairOrder.getEID());
@@ -112,10 +98,7 @@ public class RepairOrderDAO {
         int shipOut_CID = rs.getInt("ShipOut_CID");
         int EID = rs.getInt("EID");
         int shipIn_CID = rs.getInt("ShipIn_CID");
-        String shipType = rs.getString("shipType");
-        int shipOut_CID = rs.getInt("shipOut_CID");
-        int EID = rs.getInt("EID");
-        int shipIn_CID = rs.getInt("shipIn_CID");
+     
         int RID = rs.getInt("RID");
         return new RepairOrder();
     }
